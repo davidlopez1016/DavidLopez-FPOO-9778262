9c0eb715de160151c1b85a793b7de5a8924c1c5d
David Lopez <david_steven_1016@hotmail.com>
2024-09-12 01:17:15 +0000

    Este es un programa para pedir edad y nombre

---
+++ SEMANAUNO/LAB-1.py
@@ -0,0 +1,8 @@
+def get_average_and_largest():
+    name = input("ingrese su nombre")
+    age = int(input("ingrese edad "))
+
+    print(f" Me llamo {name} y tengo {age}")
+
+
+get_average_and_largest()
